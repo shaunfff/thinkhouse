cad583809fdcad1aa2a6639bb34992d1ff520606
shaunph <shaun@wove.co>
2020-03-10 13:27:34 +0000

    footer

--- js/build/script.js
+++ js/build/script.js
@@ -10,8 +10,8 @@ const callback = entries => {
 
 const options = {
   root: null,
-  rootMargin: "10% 0% 0% 0%",
-  threshold: 0.6
+  rootMargin: "10% 0% -10% 0%",
+  threshold: 0.2
 };
 
 const observer = new IntersectionObserver(callback, options);
@@ -21,31 +21,34 @@ targets.forEach(target => {
   const maskTop = target.querySelectorAll(".mask-container-top");
   const maskLeft = target.querySelectorAll(".mask-container-left");
   const maskBtn = target.querySelectorAll(".mask-container");
+  const maskRight = target.querySelectorAll(".mask-container-right");
+  const fadeDown = target.querySelectorAll(".fade-down");
   // const text = target.querySelectorAll(".text-animate");
   // const t = new SplitText(target, { type: "chars, lines" });
 
   const action = gsap
     .timeline({ paused: true })
     .from(
-      maskTop,
+      maskRight,
       {
-        clipPath: "inset(0 0 100% 0)",
+        clipPath: "inset(0 100% 0 0)",
         ease: Expo.easeInOut,
-        stagger: 0.2,
         duration: 0.8
       },
       0
     )
+
     .from(
       maskLeft,
       {
         clipPath: "inset(0 0 0 100%)",
         ease: Expo.easeInOut,
         stagger: 0.2,
-        duration: 0.8
+        duration: 0.9
       },
       0
     )
+
     // .from(t.chars, { y: -150, duration: 0.5 })
     .from(
       maskBtn,
@@ -54,7 +57,26 @@ targets.forEach(target => {
         ease: Expo.easeInOut,
         duration: 0.8
       },
-      "-=.5"
+      0
+    )
+    .from(
+      maskTop,
+      {
+        clipPath: "inset(0 0 100% 0)",
+        ease: Expo.easeInOut,
+        stagger: 0.2,
+        duration: 0.9
+      },
+      0
+    )
+    .from(
+      fadeDown,
+      {
+        y: -80,
+        opacity: 0,
+        duration: 0.9
+      },
+      0
     );
 
   target.timeline = action;
