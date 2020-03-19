cf87642c40508e9f8c5d4ffb25225160f721ac22
Shaun <shaun@wove.co>
2020-03-19 12:12:48 +0000

    cchanges

--- js/build/script.js
+++ js/build/script.js
@@ -132,45 +132,28 @@ closeBtn.addEventListener("click", function() {
 //   }
 // };
 
-// mySiema.addPagination();
-
-// window.addEventListener("resize", Siema.prototype.addPagination());
-
-// extend a Siema class by two methods
-// addDots - to create a markup for dots
-// updateDots - to update classes on dots on change callback
 class SiemaWithDots extends Siema {
   addDots() {
-    // create a contnier for all dots
-    // add a class 'dots' for styling reason
     this.dots = document.createElement("div");
     this.dots.classList.add("dots");
 
-    // loop through slides to create a number of dots
     for (let i = 0; i < this.innerElements.length; i++) {
-      // create a dot
       const dot = document.createElement("button");
 
-      // add a class to dot
       dot.classList.add("dots__item");
 
-      // add an event handler to each of them
       dot.addEventListener("click", () => {
         this.goTo(i);
       });
 
-      // append dot to a container for all of them
       this.dots.appendChild(dot);
     }
 
-    // add the container full of dots after selector
     this.selector.parentNode.insertBefore(this.dots, this.selector.nextSibling);
   }
 
   updateDots() {
-    // loop through all dots
     for (let i = 0; i < this.dots.querySelectorAll("button").length; i++) {
-      // if current dot matches currentSlide prop, add a class to it, remove otherwise
       const addOrRemove = this.currentSlide === i ? "add" : "remove";
       this.dots
         .querySelectorAll("button")
@@ -179,15 +162,12 @@ class SiemaWithDots extends Siema {
   }
 }
 
-// instantiate new extended Siema
 const mySiemaWithDots = new SiemaWithDots({
-  // on init trigger method created above
   onInit: function() {
     this.addDots();
     this.updateDots();
   },
 
-  // on change trigger method created above
   onChange: function() {
     this.updateDots();
   }
